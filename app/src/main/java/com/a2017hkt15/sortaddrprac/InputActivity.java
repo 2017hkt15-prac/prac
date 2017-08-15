fa1319829933f4f9665a868fbfb71e7942bcc583
gwmail <gwwork01@gmail.com>
2017-08-15 16:43:22 +0900

    InputActivity

---
+++ app/src/main/java/com/a2017hkt15/sortaddrprac/InputActivity.java
@@ -0,0 +1,17 @@
+package com.a2017hkt15.sortaddrprac;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by gwmail on 2017-08-15.
+ */
+
+public class InputActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_input);
+    }
+}
