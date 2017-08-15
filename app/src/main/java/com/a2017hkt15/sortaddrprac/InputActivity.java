85fab416880ee5978a98d88799b47b239c828474
gwmail <gwwork01@gmail.com>
2017-08-15 18:28:36 +0900

    InputActivity

--- app/src/main/java/com/a2017hkt15/sortaddrprac/InputActivity.java
+++ app/src/main/java/com/a2017hkt15/sortaddrprac/InputActivity.java
@@ -1,7 +1,13 @@
 package com.a2017hkt15.sortaddrprac;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+
+import com.skp.Tmap.TMapView;
 
 /**
  * Created by gwmail on 2017-08-15.
@@ -13,5 +19,21 @@ public class InputActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_input);
+
+        LinearLayout layoutForMap = (LinearLayout)findViewById(R.id.layout_for_map);
+        TMapView tmapview = new TMapView(this);
+
+        tmapview.setSKPMapApiKey("d78cbfb1-f9ee-3742-af96-bf845debb9ab");
+        tmapview.setCompassMode(true);
+        tmapview.setIconVisibility(true);
+        tmapview.setZoomLevel(15);
+        tmapview.setMapType(TMapView.MAPTYPE_STANDARD);
+        tmapview.setLanguage(TMapView.LANGUAGE_KOREAN);
+        tmapview.setTrackingMode(true);
+        tmapview.setSightVisible(true);
+        tmapview.setTrafficInfo(true);
+        layoutForMap.addView(tmapview);
+        setContentView(layoutForMap);
+
     }
 }
