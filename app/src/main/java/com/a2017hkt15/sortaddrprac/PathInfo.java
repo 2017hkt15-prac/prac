58da6e99ba7e8f0b4d7733a7b2ae0c6c55179fb2
gkatkdgur12 <gkatkdgur12@naver.com>
2017-08-17 14:31:09 +0900

    pathInfo 클래스

---
+++ app/src/main/java/com/a2017hkt15/sortaddrprac/PathInfo.java
@@ -0,0 +1,16 @@
+package com.a2017hkt15.sortaddrprac;
+
+/**
+ * Created by 함상혁입니다 on 2017-08-17.
+ */
+
+public class PathInfo {
+    int[] pathRoute;
+    int pathLength;
+
+    public PathInfo(int[] pr,int pl)
+    {
+        pathRoute = pr;
+        pathLength =pl;
+    }
+}
