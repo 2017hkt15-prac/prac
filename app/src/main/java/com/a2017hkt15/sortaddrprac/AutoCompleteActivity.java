78b27b8ccf8afd0f2e9ff9ba23b37f9f72787073
house1998 <house1998@naver.com>
2017-08-19 17:25:15 +0900

    입력 버튼 삭제

--- app/src/main/java/com/a2017hkt15/sortaddrprac/AutoCompleteActivity.java
+++ app/src/main/java/com/a2017hkt15/sortaddrprac/AutoCompleteActivity.java
@@ -26,6 +26,8 @@ public class AutoCompleteActivity extends AppCompatActivity {
     String address_send;
     static double lat;
     static double lon;
+    String address;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -42,7 +44,7 @@ public class AutoCompleteActivity extends AppCompatActivity {
         toolbar.setTitleTextColor(Color.WHITE);
         toolbar.setSubtitleTextColor(ContextCompat.getColor(AutoCompleteActivity.this, R.color.colorSubtitle));
 
-        if(getSupportActionBar() != null) {
+        if (getSupportActionBar() != null) {
             getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         }
 
@@ -54,10 +56,10 @@ public class AutoCompleteActivity extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(android.view.MenuItem item) {
-        switch(item.getItemId()) {
+        switch (item.getItemId()) {
             case android.R.id.home:
-                Intent intent = new Intent(AutoCompleteActivity.this,InputActivity.class);
-                setResult(RESULT_CANCELED,intent);
+                Intent intent = new Intent(AutoCompleteActivity.this, InputActivity.class);
+                setResult(RESULT_CANCELED, intent);
                 finish();
                 break;
         }
@@ -68,70 +70,66 @@ public class AutoCompleteActivity extends AppCompatActivity {
     public void onClick(View v) {
 
         final ArrayList<String> addressList = new ArrayList<>();
-        String address;
+       // String address;
         final EditText editText = (EditText) findViewById(R.id.edit);
         address = editText.getText().toString();
 
         //address가 장소 이름 edittext로 부터 받아옴
         //address를 키워드로 해당 리스트를 받아옴
-        tMapdata.autoComplete(address, new TMapData.AutoCompleteListenerCallback() {
-            @Override
-            public void onAutoComplete(ArrayList<String> poiltem) {
-                for (int i = 0; i < poiltem.size(); i++) {
-                    //리스트값을 addresslist로 add
-                    addressList.add(poiltem.get(i));
-                }
-                runOnUiThread(new Runnable() {
-                    public void run() {
-                        ArrayAdapter<String> Adapter;
-                        Adapter = new ArrayAdapter<String>(AutoCompleteActivity.this, android.R.layout.simple_list_item_1, addressList);
-                        ListView list = (ListView) findViewById(R.id.list);
-                        Adapter.notifyDataSetChanged();
-                        list.setAdapter(Adapter);
-                        list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-                            @Override
-                            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                                Toast.makeText(AutoCompleteActivity.this, addressList.get(position), Toast.LENGTH_SHORT).show();
-                                editText.setText(addressList.get(position));
-                                //listview에서 클릭한 값으로 edittext값 변경
-                                address_send = editText.getText().toString();
+        runOnUiThread(new Runnable() {
+            public void run() {
+                tMapdata.autoComplete(address, new TMapData.AutoCompleteListenerCallback() {
+                    @Override
+                    public void onAutoComplete(ArrayList<String> poiltem) {
+                        for (int i = 0; i < poiltem.size(); i++) {
+                            addressList.add(poiltem.get(i));
+                        }
+                        runOnUiThread(new Runnable() {
+                            public void run() {
+                                ArrayAdapter<String> Adapter;
+                                Adapter = new ArrayAdapter<String>(AutoCompleteActivity.this, android.R.layout.simple_list_item_1, addressList);
+                                ListView list = (ListView) findViewById(R.id.list);
+                                Adapter.notifyDataSetChanged();
+                                list.setAdapter(Adapter);
+                                list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                                    @Override
+                                    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                                        editText.setText(addressList.get(position));
+                                        address_send = editText.getText().toString();
+                                        Intent intent1 = getIntent();
+                                        //위 경도값도 같이 보내기
+                                        //받는 곳 inputActivity에서 배열로 저장 후 마커 찍기
+                                        AddressInfo info = new AddressInfo();
+                                        position = intent1.getIntExtra("position", 0);
+                                        Intent intent = new Intent(AutoCompleteActivity.this, InputActivity.class);
+                                        runOnUiThread(new Runnable() {
+                                            public void run() {
+                                                TMapData tdata = new TMapData();
+                                                tdata.findAllPOI(address_send, new TMapData.FindAllPOIListenerCallback() {
+                                                    @Override
+                                                    public void onFindAllPOI(ArrayList<TMapPOIItem> poiItem) {
+                                                        String[] array;
+                                                        TMapPOIItem item2 = poiItem.get(poiItem.size() - 1);
+                                                        array = item2.getPOIPoint().toString().split(" ");
+                                                        lat = Double.parseDouble(array[1]);
+                                                        lon = Double.parseDouble(array[3]);
+                                                    }
+                                                });
+                                            }
+                                        });
+
+                                        intent.putExtra("address_name", address_send);
+                                        intent.putExtra("position", position);
+                                        setResult(RESULT_OK, intent);
+                                        finish();
+                                    }
+                                });
                             }
                         });
                     }
                 });
             }
         });
-    }
 
-    public void onPass(View v) {
-        //address_send 검색해서 받아온 주소이름
-        Log.i("onPass", address_send);
-        int position;
-        Intent intent1 = getIntent();
-        //위 경도값도 같이 보내기
-        //받는 곳 inputActivity에서 배열로 저장 후 마커 찍기
-        AddressInfo info = new AddressInfo();
-        position = intent1.getIntExtra("position",0);
-        Intent intent = new Intent(AutoCompleteActivity.this,InputActivity.class);
-        runOnUiThread(new Runnable() {
-                          public void run() {
-                              TMapData tdata = new TMapData();
-                              tdata.findAllPOI(address_send, new TMapData.FindAllPOIListenerCallback() {
-                                  @Override
-                                  public void onFindAllPOI(ArrayList<TMapPOIItem> poiItem) {
-                                      String[] array;
-                                      TMapPOIItem item2 = poiItem.get(poiItem.size() - 1);
-                                      array = item2.getPOIPoint().toString().split(" ");
-                                      lat = Double.parseDouble(array[1]);
-                                      lon = Double.parseDouble(array[3]);
-                                  }
-                              });
-                          }
-                      });
-
-        intent.putExtra("address_name",address_send);
-        intent.putExtra("position",position);
-        setResult(RESULT_OK,intent);
-        finish();
     }
 }
