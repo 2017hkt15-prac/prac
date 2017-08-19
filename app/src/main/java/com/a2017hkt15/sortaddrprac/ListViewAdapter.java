fec81acb03be8090a103b2bd64a71fc802aa35a7
house1998 <house1998@naver.com>
2017-08-19 15:12:45 +0900

    ListAdapter 수정 List 버튼 눌렀을때 리스트 한줄 삭제와 함께 Arraylist index 값도 삭제

--- app/src/main/java/com/a2017hkt15/sortaddrprac/ListViewAdapter.java
+++ app/src/main/java/com/a2017hkt15/sortaddrprac/ListViewAdapter.java
@@ -25,17 +25,17 @@ import java.util.ArrayList;
 public class ListViewAdapter extends BaseAdapter {
     private InputActivity inputActivity;
     // Adapter에 추가된 데이터를 저장하기 위한 ArrayList
-    private ArrayList<ListViewWay> listViewWayList = new ArrayList<>() ;
+    private ArrayList<ListViewWay> listViewWayList = new ArrayList<>();
 
     // ListViewAdapter의 생성자
     public ListViewAdapter(InputActivity inputActivity) {
-        this.inputActivity=inputActivity;
+        this.inputActivity = inputActivity;
     }
 
     // Adapter에 사용되는 데이터의 개수를 리턴. : 필수 구현
     @Override
     public int getCount() {
-        return listViewWayList.size() ;
+        return listViewWayList.size();
     }
 
     // position에 위치한 데이터를 화면에 출력하는데 사용될 View를 리턴. : 필수 구현
@@ -52,9 +52,9 @@ public class ListViewAdapter extends BaseAdapter {
         }
 
         // 화면에 표시될 View(Layout이 inflate된)으로부터 위젯에 대한 참조 획득
-        ImageButton imageButtonDelete = (ImageButton) convertView.findViewById(R.id.imageButtonDelete) ;
-        TextView titleTextView = (TextView) convertView.findViewById(R.id.textViewTitle) ;
-        EditText wayEditText = (EditText) convertView.findViewById(R.id.editTextWay) ;
+        ImageButton imageButtonDelete = (ImageButton) convertView.findViewById(R.id.imageButtonDelete);
+        TextView titleTextView = (TextView) convertView.findViewById(R.id.textViewTitle);
+        EditText wayEditText = (EditText) convertView.findViewById(R.id.editTextWay);
 
         // Data Set(listViewItemList)에서 position에 위치한 데이터 참조 획득
         ListViewWay listViewWay = listViewWayList.get(position);
@@ -63,19 +63,22 @@ public class ListViewAdapter extends BaseAdapter {
         imageButtonDelete.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(pos != 0) {
-                    listViewWayList.remove(pos);
-                    //button_pos을 넘겨줌
-                    inputActivity.setButton_pos(pos);
+                if (pos != 0 && pos + 1 <= inputActivity.getAddressInfo_array().size()) {
+
                     //해당 arraylist의 값을 삭제
                     inputActivity.getAddressInfo_array().remove(pos);
                     //해당 장소의 마커 삭제
                     inputActivity.getMarkerController().removeMarker(pos);
-
+                   // listViewWayList.remove(pos);
+                   // Variable.numberOfLine--;
+                   // notifyDataSetChanged();    삭제해도 되는 듯
+                }
+                if (pos >= inputActivity.getAddressInfo_array().size() + 1) {
+                    listViewWayList.remove(pos);
                     Variable.numberOfLine--;
                     notifyDataSetChanged();
                 }
-                if(pos>inputActivity.getAddressInfo_array().size()){
+                else {
                     listViewWayList.remove(pos);
                     Variable.numberOfLine--;
                     notifyDataSetChanged();
@@ -88,10 +91,10 @@ public class ListViewAdapter extends BaseAdapter {
             @Override
             public void onClick(View v) {
                 //TODO: 입력하는 Activity로 이동
-                Intent intent = new Intent(context,AutoCompleteActivity.class);
+                Intent intent = new Intent(context, AutoCompleteActivity.class);
                 //position값 보냄
-                intent.putExtra("position",pos);
-                inputActivity.startActivityForResult(intent,1);   //pos
+                intent.putExtra("position", pos);
+                inputActivity.startActivityForResult(intent, 1);   //pos
             }
         });
 
@@ -106,7 +109,7 @@ public class ListViewAdapter extends BaseAdapter {
     // 지정한 위치(position)에 있는 데이터와 관계된 아이템(row)의 ID를 리턴. : 필수 구현
     @Override
     public long getItemId(int position) {
-        return position ;
+        return position;
     }
 
     // 지정한 위치(position)에 있는 데이터 리턴 : 필수 구현
